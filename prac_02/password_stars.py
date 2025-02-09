081afd1381a60d45013a9764619f5b5f78021826
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-09 16:09:19 +0800

    implement password input with length validation

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,16 @@
 MIN_LENGTH = 7
 
+
 def main():
     password = get_password()
     print_password_asterisks(password)
+
+
+def get_password():
+    password = input("Please enter your password:")
+
+    while len(password) < MIN_LENGTH:
+        print("Invalid password. At least 7 characters long.")
+        password = input("Please enter your password:")
+
+    return password
