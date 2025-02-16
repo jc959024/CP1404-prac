f784fa381618b75002d8bdae01689eb8f4454186
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-16 15:03:17 +0800

    Added functionality to read name from file and print greeting

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,3 +1,7 @@
 name = input("Please enter your name: ")
 with open("name.txt", "w") as file:
     file.write(name)
+
+with open("name.txt", "r") as file:
+    name = file.read().strip()
+print(f"Hi {name}!")
