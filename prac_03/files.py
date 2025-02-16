54256f86a69f6c7413da177298c2c56d6cd63104
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-16 15:04:34 +0800

    Added functionality to read first two numbers from file and compute sum

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,3 +5,10 @@ with open("name.txt", "w") as file:
 with open("name.txt", "r") as file:
     name = file.read().strip()
 print(f"Hi {name}!")
+
+with open("numbers.txt", "r") as file:
+    first_number = int(file.readline().strip())
+    second_number = int(file.readline().strip())
+
+result = first_number + second_number
+print(result)
