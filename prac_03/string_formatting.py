f3205863bccf3507bf7ef75797110ac321820d68
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-16 13:41:56 +0800

    Added currency formatting examples using str.format() and f-strings

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -12,3 +12,7 @@ print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
 
 # And with f-string formatting, introduced in Python 3.6 (do this)
 print(f"My {name} was first made in {year} (that's right, {year}!)")
+
+# Formatting currency (grouping with comma, 2 decimal places):
+print("My {} would cost ${:,.2f}".format(name, cost))  # str.format version
+print(f"My {name} would cost ${cost:,.2f}")  # preferred f-string version
