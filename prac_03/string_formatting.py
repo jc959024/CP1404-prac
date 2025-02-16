1a6d4fccd4a378378e59f5736f3ad18648309041
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-16 13:41:36 +0800

    Refactored string formatting examples to prefer f-strings over str.format()

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -9,3 +9,6 @@ print("My guitar: " + name + ", first made in " + str(year))
 print("My guitar: {}, first made in {}".format(name, year))
 print("My guitar: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
+
+# And with f-string formatting, introduced in Python 3.6 (do this)
+print(f"My {name} was first made in {year} (that's right, {year}!)")
