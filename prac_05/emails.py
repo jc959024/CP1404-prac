23b8a7ea5af48fb99036e1d759e99961503f7ac2
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-27 15:42:03 +0800

    while loop

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,3 +8,12 @@ Actual:    minutes
 def main():
     email_to_name = {}
     email = input("Email: ")
+
+
+while email != "":
+    name = extract_name_from_email(email)
+    is_correct = input(f"Is your name {name}? (Y/n) ").strip().lower()
+    if is_correct not in ["", "y", "yes"]:
+        name = input("Name: ").title()
+    email_to_name[email] = name
+    email = input("Email: ")
