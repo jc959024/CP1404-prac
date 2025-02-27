075fecd6e4d2dc67675beeb36d9e537be782b1ff
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-27 15:13:56 +0800

    Make a constant dictionary of 10 color names

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+Hexadecimal Colour Code
+"""
+
+HEX_COLOURS = {
+    "AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Aquamarine": "#7fffd4",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#ffebcd",
+    "Blue": "#0000ff"
+}
+
