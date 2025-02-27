cf0c55d073ffa86f8184bfbd068440d420fa02cb
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-02-27 15:56:39 +0800

    Complete prac 5 emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,22 +1,30 @@
 """
-Word Occurrences
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   27 minutes
 """
 
 
 def main():
     email_to_name = {}
     email = input("Email: ")
+    while email != "":
+        name = extract_name_from_email(email)
+        is_correct = input(f"Is your name {name}? (Y/n) ").strip().lower()
+        if is_correct not in ["", "y", "yes"]:
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
-while email != "":
-    name = extract_name_from_email(email)
-    is_correct = input(f"Is your name {name}? (Y/n) ").strip().lower()
-    if is_correct not in ["", "y", "yes"]:
-        name = input("Name: ").title()
-    email_to_name[email] = name
-    email = input("Email: ")
+def extract_name_from_email(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
 
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+
+if __name__ == "__main__":
+    main()
