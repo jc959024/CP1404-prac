81a366d77f22672408c252857ab3795a8adee621
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 15:40:22 +0800

    Implement the __str__() method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -15,3 +15,7 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing.lower() == 'dynamic'
+
+    def __str__(self):
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"First appeared in {self.year}")
