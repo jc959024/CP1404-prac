6135f252df1ef0e0d99d8c21eb93f91d0ba050a5
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 15:39:33 +0800

    Implement the is_dynamic() method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,8 +8,10 @@ Actual:    minutes
 class ProgrammingLanguage:
 
     def __init__(self, name, typing, reflection, year):
-
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        return self.typing.lower() == 'dynamic'
