8a50dceeb5603b46af1dbd24578d0bea5683d7fc
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 15:00:39 +0800

    Complete the "car" exercise

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,27 +1,14 @@
-"""CP1404/CP5632 Practical - Car class example."""
-
-
 class Car:
-    """Represent a Car object."""
 
-    def __init__(self, fuel=0):
-        """Initialise a Car instance.
-
-        fuel: float, one unit of fuel drives one kilometre
-        """
+    def __init__(self, name, fuel=0):
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
     def add_fuel(self, amount):
-        """Add amount to the car's fuel."""
         self.fuel += amount
 
     def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
         if distance > self.fuel:
             distance = self.fuel
             self.fuel = 0
@@ -29,3 +16,7 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
+
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
+
