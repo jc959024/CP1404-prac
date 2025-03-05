2481abdc15f5c736967a449063bf9a040d3f884e
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 16:58:23 +0800

    Create guitars.py to collect and store guitar details

---
+++ prac_06/guitars.py
@@ -0,0 +1,12 @@
+def main():
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
