1bd81b811f4b442a765d8bb47aaa2a88f16e4ce5
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 15:38:37 +0800

    Create a ProgrammingLanguage class with attributes and implement the __init__ constructor

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:    minutes
+"""
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
