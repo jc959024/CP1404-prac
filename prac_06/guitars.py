8873c639402d657f772c1f66db6d88e2740a642f
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-05 16:59:21 +0800

    Add display functionality for stored guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -10,3 +10,11 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added.\n")
         name = input("Name: ")
+
+    if guitars:
+        print("\nThese are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :( Maybe buy one?")
