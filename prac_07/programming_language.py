4cad987fd549b99ec53b895b1cfe08739040b36e
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-03-23 15:52:52 +0800

    Modifying the ProgrammingLanguage Class

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,19 +7,18 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
-        """Construct a ProgrammingLanguage from the given values."""
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
-        """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
 
     def is_dynamic(self):
-        """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
 
