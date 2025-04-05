1583ad65fa6a12152f6f9f76fdcc14bb5d49a722
jc959024 <huayu.zhong@my.jcu.edu.au>
2025-04-05 15:39:24 +0800

    Adjust the code in taxi.py related to what should be adjusted

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -2,17 +2,18 @@
 CP1404/CP5632 Practical
 Car class
 """
-from prac_09.car import Car
+
+from car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+
     price_per_km = 1.23
 
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -20,8 +21,9 @@ class Taxi(Car):
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        """Return the price for the taxi trip, rounded to nearest 10c."""
+        fare = self.price_per_km * self.current_fare_distance
+        return round(fare, 1)
 
     def start_fare(self):
         """Begin a new fare."""
